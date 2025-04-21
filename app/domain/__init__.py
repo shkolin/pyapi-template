f1bba1d6a484f1c233256d96eb7d57b94da0553e
Vitalii Shkolin <vitalii@shkolin.net.ua>
2025-04-21 13:05:36 +0300

    fix: linter issues

--- app/domain/__init__.py
+++ app/domain/__init__.py
@@ -1,6 +1,6 @@
 # This is needed for automatic generation of migrations in alembic
-from app.domain.base import Base
-from app.domain.event_log import EventLog
-from app.domain.user.login import LoginResetRequest
-from app.domain.user.password import PasswordResetRequest
-from app.domain.user.user import User
+from app.domain.base import Base  # noqa: F401
+from app.domain.event_log import EventLog  # noqa: F401
+from app.domain.user.login import LoginResetRequest  # noqa: F401
+from app.domain.user.password import PasswordResetRequest  # noqa: F401
+from app.domain.user.user import User  # noqa: F401
